f9d9203b463f96380db1e7493201da813d03d1f2
EoGeunHyeok <eghsamo@naver.com>
2024-06-10 15:38:52 +0900

    secret Key

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.jwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtApplicationTests {
 
+	@Value("${custom.jwt.secretKey}")
+	private String secretKey;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("시크릿 키 존재 여부 체크")
+	void test1() {
+		assertThat(secretKey).isNotNull(); // 정확성을 확인하기위한 도구
 	}
 
 }
