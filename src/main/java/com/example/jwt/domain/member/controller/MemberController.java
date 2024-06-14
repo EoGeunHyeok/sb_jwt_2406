5a54221de08d6f993ff61f62906c89698447ebc9
EoGeunHyeok <eghsamo@naver.com>
2024-06-14 16:19:05 +0900

    add member/me

--- src/main/java/com/example/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jwt/domain/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.jwt.domain.member.controller;
 
+import com.example.jwt.domain.member.entity.Member;
 import com.example.jwt.domain.member.service.MemberService;
 import com.example.jwt.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,11 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.util.MimeTypeUtils.ALL_VALUE;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -36,7 +35,7 @@ public class MemberController {
         private final String accessToken;
     }
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", consumes = APPLICATION_JSON_VALUE)
     // @ResponseBody -> RestController 달면 굳이 안써도 상관없음
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
 
@@ -52,7 +51,22 @@ public class MemberController {
                 "액서스 토큰이 생성되었습니담.",
                 new LoginResponse(accessToken)
         );
-
     }
+    @Getter
+    @AllArgsConstructor
+    public static class MeResponse{
+        private final Member member;
+    }
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me() {
+        Member member = memberService.findByUsername("user1").get();
+
+        return RsData.of(
+                "S-2",
+                "성공",
+                new MeResponse(member)
+        );
+    }
+
 }
 
